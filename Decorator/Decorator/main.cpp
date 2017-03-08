4fd83ab48b3a9ce2028f3e1f7e6318b1fed49c6f
wyatt <wyatth@gmail.com>
2017-03-08 22:48:06 +0800

    decorator

---
+++ Decorator/Decorator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Decorator
+//
+//  Created by wyatt on 2017/3/8.
+//  Copyright © 2017年 wyatt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
